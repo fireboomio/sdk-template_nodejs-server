b8c5be22c8bfe7d5703a1ba774d598386b297ccf
erguotou <erguotou525@gmail.com>
2023-09-15 08:22:39 +0800

    feat: use npm package

--- files/src/utils.ts
+++
@@ -1,5 +0,0 @@
-export function replaceUrl(url: string, variables: Record<string, string | number>) {
-  return Object.keys(variables).reduce<string>((str, key) => {
-    return str.replace(`{${key}}`, variables[key].toString())
-  }, url)
-}
